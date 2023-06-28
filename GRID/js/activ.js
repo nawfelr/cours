39ad9f5707fccafaa6fdf12acdeceaff05bfbecc
nawfelr <nawfelrahmani@hotmail.com>
2023-06-28 16:55:50 +0200

    first

---
+++ GRID/js/activ.js
@@ -0,0 +1,7 @@
+// Navigation avec les liens actifs sur la page en cours
+
+// 1- Ajouter l'id #activ_nav sur la balise <nav>
+
+// 2- verifier que la class .activ est bien sur le fichier css
+
+// 3- créer la fonction setActiv() pour ajouter la class .activ sur le lien URL en cours
